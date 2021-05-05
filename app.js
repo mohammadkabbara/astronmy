e5381802734679718a57143b85cf8d47226581b1
MohammadKabbara <>
2021-05-05 12:10:59 +0000

    uploading

---
+++ app.js
@@ -0,0 +1,21 @@
+alert('hello');
+
+document.write('<p>'+'iam came from replit'+'</p>');
+console.log(5=='5');
+console.log(5=='50');
+var astronomy=prompt('do you like stars?');
+if(astronomy=='yes'){
+  document.write('you are in your place');
+}else{document.write('find anthoer place')};
+ console.log('hello astronomy world');
+
+var yes = true;
+if(yes = true){
+  console.log('you are in your place');
+}else{
+  console.log('find anthoer place' );
+}
+
+
+
+
