04f8ff17c49f4e7c575aea38451a99019936faaf
MohammadKabbara <>
2021-05-06 11:06:10 +0000

    pushing

---
+++ app.js
@@ -0,0 +1,39 @@
+alert('hello');
+
+document.write('<p>'+'iam came from replit'+'</p>');
+console.log(5=='5');
+console.log(5=='50');
+var astronomy=prompt('do you like stars?');
+if(astronomy=='yes'){
+  document.write('you are in your place');
+}else{document.write('find anthoer place')};
+ console.log('hello astronomy world');
+
+var yes = true;
+if(yes = true){
+  console.log('you are in your place')
+}else{
+  console.log('find anthoer place');
+}
+
+
+ for(var i=0; i<2; i++){
+   console.log('you can do every thing');
+   }
+   for(var s=0;s<1;s++){
+      document.write('<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWk9XQTn3s1-G5j9bR1Rthh6aq0JftXHRmNw&usqp=CAU">');
+}
+
+var astronomy = prompt('do you like stars and unvers?');
+
+while (astronomy !== 'yes' && astronomy  !== 'no') {
+  astronomy = prompt('yes or no just pls');
+}
+  
+
+  
+
+
+
+
+
